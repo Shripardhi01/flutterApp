6648f59aa74d51794a08f2ee708673eead7478cf
Shrikant Pardhi <dynast.dev@gmail.com>
2020-08-05 13:00:38 +0530

    spliited widget and added vertical scroll

--- Expencer/lib/main.dart
+++ Expencer/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:intl/intl.dart';
-import './transaction.dart';
+
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
+import './widgets/user_transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   static const String _title = 'Expense Tracker';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,117 +19,24 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text(_title),
         ),
-        body: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key}) : super(key: key);
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'new Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'clothes',
-      amount: 50.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't3',
-      title: 'new poppy',
-      amount: 85.15,
-      date: DateTime.now(),
-    ),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      // mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Container(
-          width: double.infinity,
-          child: Card(
-            color: Colors.blue,
-            child: Text('Chart!'),
-            elevation: 10,
-          ),
-        ),
-        Card(
-          elevation: 5,
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(labelText: 'Title'),
+        body: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('Chart!'),
+                  elevation: 10,
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Amount'),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Column(
-          children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 10,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(5),
-                    child: Text(
-                      '\$${tx.amount}', // '\$ ' + tx.amount.toString(),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        DateFormat('yyyy-MMM-dd HH:MM').format(tx.date),
-                        // tx.date.toString(),
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
               ),
-            );
-          }).toList(),
+              UserTransactions(),
+            ],
+          ),
         ),
-      ],
+      ),
     );
   }
 }
