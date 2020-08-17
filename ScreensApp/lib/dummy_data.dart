923dbbee6aae2fecec96021711371553a5ab5b22
Shrikant Pardhi <dynast.dev@gmail.com>
2020-08-17 16:48:40 +0530

    added page route

---
+++ ScreensApp/lib/dummy_data.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Chainese',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Marathi Katta',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Nagpurian',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Akash Special',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Quick Mesh',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Papa',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Shrikant',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c11',
+    title: 'Harshaj Special',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c12',
+    title: 'Indian',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c13',
+    title: 'Baiko Special',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c14',
+    title: 'Chainese',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c15',
+    title: 'Marathi Katta',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c16',
+    title: 'Nagpurian',
+    color: Colors.teal,
+  ),
+];
---
+++ Temp/android/app/src/main/kotlin/com/example/Temp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Temp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
