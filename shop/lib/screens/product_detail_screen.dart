39a91e5cb1fef0522fc94407d96b991e9f832643
Shrikant Pardhi <dynast.dev@gmail.com>
2020-08-21 22:50:38 +0530

    added cart, orders and drawer to app

--- shop/lib/screens/product_detail_screen.dart
+++ shop/lib/screens/product_detail_screen.dart
@@ -2,22 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
+import '../widgets/app_drawer.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
   // final double price;
-  // ProductDetailScreen(this.title, this.price);
 
+  // ProductDetailScreen(this.title, this.price);
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context).settings.arguments as String;
-    final loadedProducts =
-        Provider.of<Products>(context, listen: false).findById(productId);
+    final productId =
+        ModalRoute.of(context).settings.arguments as String; // is the id!
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(loadedProducts.title),
+        title: Text(loadedProduct.title),
+      ),
+      drawer: AppDrawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              loadedProduct.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          ],
+        ),
       ),
     );
   }
